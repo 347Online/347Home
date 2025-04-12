da71c07e47a282861cd3542ca982db9e3d74296e
347Online | Katie Janzen <katiejanzen@347online.me>
2025-04-12 15:51:47 -0500

    Initial commit
    Port over starting config from 347OS

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+						file_ignore_patterns = {
+							"^./.git/",
+							"^node_modules/",
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+}
---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,13 @@
+vim.opt.linebreak = true
+vim.opt.foldenable = false
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.cursorline = true
