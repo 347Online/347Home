107653a5913b85621169bdd3459160aab44d0446
347Online | Katie Janzen <katiejanzen@347online.me>
2025-04-16 00:51:31 -0500

    Mac: Add system settings to dock

--- .setup/mac-dock.zsh
+++ .setup/mac-dock.zsh
@@ -1,6 +1,6 @@
 dock_item() {
   defaults write com.apple.dock persistent-apps -array-add \
-  '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'$1'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+  "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$1</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 }
 
 defaults write com.apple.dock tilesize 64
@@ -18,5 +18,6 @@ dock_item "/Applications/Broadcasts.app"
 dock_item "/Applications/Overcast.app" # TODO: Personal only
 dock_item "/System/Applications/Messages.app" # TODO: Personal only
 dock_item "/Applications/Ghostty.app"
+dock_item "/System/Applications/System Settings.app"
 
 killall Dock
