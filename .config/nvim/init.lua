b9c06bb0b4c85bbf83bfd332705f49c1ecf9a265
347Online | Katie Janzen <katiejanzen@347online.me>
2025-04-21 08:19:52 -0500

    Nvim: Highlight on yank

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,9 @@ vim.g.mapleader = ","
 require("config.keymaps")
 require("config.lazy")
 require("config.options")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
